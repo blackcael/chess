f2e90bd335f394d55a13fb9a88ce5cf6cb795629
blackcael <blackcael@gmail.com>
2024-05-02 09:59:08 -0600

    Added SimpleJavaClass for Testing

---
+++ SimpleJavaClass.java
@@ -0,0 +1,10 @@
+///create a file via
+public class SimpleJavaClass{
+    public static void main(String [] args){
+        System.out.println("Hello BYU!");
+    }
+}
+// a function inside a class is not a function, it is a method
+
+//to fix things, invalidate cache and restart (no need to click any boxes)
+
