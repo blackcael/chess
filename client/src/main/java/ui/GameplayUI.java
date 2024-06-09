1bf9009f99da993d121228248d61a19919296c63
blackcael <blackcael@gmail.com>
2024-06-08 19:55:50 -0600

    finished first draft of chessboardprinter

--- client/src/main/java/ui/GameplayUI.java
+++ client/src/main/java/ui/GameplayUI.java
@@ -5,32 +5,44 @@ import chess.*;
 public class GameplayUI {
 
     private static final int BOARD_SIZE_IN_SQUARES = 8;
-    private static final int SQUARE_SIZE_IN_CHARS = 3;
+    private static final int SQUARE_SIZE_IN_CHARS = 1;
     private static final String SET_LIGHT_COLOR = EscapeSequences.SET_BG_COLOR_LIGHT_GREY;
     private static final String SET_DARK_COLOR = EscapeSequences.SET_BG_COLOR_DARK_GREEN;
+    private static final String SET_BORDER_COLOR = EscapeSequences.SET_BG_COLOR_DARK_GREY;
 
     public static void drawBoards(ChessBoard board){
-        drawSingleBoard(board);
+        drawWhitePerspectiveBoard(board);
+        System.out.println(); //draws blank line
+        drawBlackPerspectiveBoard(board);
     }
 
     private static void drawWhitePerspectiveBoard(ChessBoard board){
+        drawColumnIndices();
         for(int rowIter = 0; rowIter < BOARD_SIZE_IN_SQUARES; rowIter ++){
+            drawBorderSquare(Integer.toString(rowIter));
             for(int colIter = 0; colIter < BOARD_SIZE_IN_SQUARES; colIter ++){
                 ChessPosition position = new ChessPosition(rowIter + 1, colIter + 1);
                 drawBoardSquare(getSquareColor(rowIter, colIter), board.getPiece(position));
             }
+            drawBorderSquare(Integer.toString(rowIter));
+
             System.out.println(EscapeSequences.RESET_BG_COLOR);
         }
+        drawColumnIndices();
     }
 
     private static void drawBlackPerspectiveBoard(ChessBoard board){
-        for(int rowIter = 0; rowIter < BOARD_SIZE_IN_SQUARES; rowIter ++){
-            for(int colIter = 0; colIter < BOARD_SIZE_IN_SQUARES; colIter ++){
+        drawReverseColumnIndices();
+        for(int rowIter = BOARD_SIZE_IN_SQUARES-1; rowIter >= 0; rowIter --){
+            drawBorderSquare(Integer.toString(rowIter));
+            for(int colIter = BOARD_SIZE_IN_SQUARES-1; colIter >= 0; colIter --){
                 ChessPosition position = new ChessPosition(rowIter + 1, colIter + 1);
                 drawBoardSquare(getSquareColor(rowIter, colIter), board.getPiece(position));
             }
+            drawBorderSquare(Integer.toString(rowIter));
             System.out.println(EscapeSequences.RESET_BG_COLOR);
         }
+        drawReverseColumnIndices();
     }
 
     private static String getSquareColor(int row, int col){
@@ -73,8 +85,22 @@ public class GameplayUI {
         return null;
     }
 
-    private static void drawBorder(){
+    private final static String[] COLUMN_INDEX_STRINGS= {EscapeSequences.EMPTY, "a", "b", "c", "d", "e", "f", "g", "h", EscapeSequences.EMPTY};
 
+    private static void drawColumnIndices(){
+        for(int i = 0; i < 10; i++){
+            drawBorderSquare(COLUMN_INDEX_STRINGS[i]);
+        }
+    }
+
+    private static void drawReverseColumnIndices(){
+        for(int i = 9; i >= 0; i--){
+            drawBorderSquare(COLUMN_INDEX_STRINGS[i]);
+        }
+    }
+
+    private static void drawBorderSquare(String singleChar){
+        System.out.print(SET_BORDER_COLOR + singleChar);
     }
 
 
