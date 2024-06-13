42a294db4fc44984f7e3cdf12a335dedd2471964
blackcael <blackcael@gmail.com>
2024-06-13 17:23:06 -0600

    createdService.leave(unfinished)

--- server/src/main/java/handler/WebSocketMessageHandler.java
+++ server/src/main/java/handler/WebSocketMessageHandler.java
@@ -3,14 +3,15 @@ package handler;
 import dataaccess.Database;
 import service.WebsocketServices;
 import websocket.commands.UserGameCommand;
+import websocket.messages.ServerMessage;
 
 public class WebSocketMessageHandler {
     public static String handleMessage(Database database, String message){
         //1. parse down the message, figure out which type of command
         UserGameCommand userGameCommand; //parseThatHomie
         WebsocketServices websocketService = new WebsocketServices(database);
-        websocketService.service(userGameCommand);
-
+        ServerMessage serverMessage = websocketService.service(userGameCommand);
+        //turn servermessage back into a string and send it back
 
         return "peepee";
     }
--- server/src/main/java/service/WebsocketServices.java
+++ server/src/main/java/service/WebsocketServices.java
@@ -1,10 +1,15 @@
 package service;
 
+import chess.ChessGame;
 import dataaccess.DataAccessException;
 import dataaccess.Database;
 import intermediary.InvalidAuthException;
+import model.GameData;
 import server.Server;
+import websocket.commands.LeaveCommand;
 import websocket.commands.UserGameCommand;
+import websocket.messages.ErrorMessage;
+import websocket.messages.NotificationMessage;
 import websocket.messages.ServerMessage;
 
 public class WebsocketServices extends BaseService{
@@ -23,19 +28,32 @@ public class WebsocketServices extends BaseService{
                 case RESIGN -> resign(command);
             };
         }catch (InvalidAuthException e) {
-            return ("Error: Invalid Message Dawg");
+            return new ErrorMessage(ServerMessage.ServerMessageType.ERROR, "Error: Invalid AuthToken");
         } catch (DataAccessException e) {
-            throw new RuntimeException(e);
+            return new ErrorMessage(ServerMessage.ServerMessageType.ERROR, "Error: Data Access Exception");
         }
     }
 
     private ServerMessage connect(UserGameCommand command){
+        //honestly not super sure what to do here...
 
     }
 
-    private ServerMessage leave(UserGameCommand command){
-
-        //
+    private ServerMessage leave(LeaveCommand leaveCommand) throws DataAccessException {
+        String username = authDataBase.getAuth(leaveCommand.getAuthString()).username();
+        GameData gameData = gameDataBase.getGame(leaveCommand.getGameID());
+        String updatedWhiteUsername = gameData.whiteUsername();
+        String updatedBlackUsername = gameData.blackUsername();
+        if(updatedWhiteUsername.equals(username)){
+            updatedWhiteUsername = null; //null instead of "null" could cause issue
+        }
+        if(updatedBlackUsername.equals(username)){
+            updatedBlackUsername = null;
+        }
+        GameData updatedGameData =new GameData(gameData.gameID(), updatedWhiteUsername, updatedBlackUsername, gameData.gameName(), gameData.game());
+        gameDataBase.updateGame(updatedGameData);
+        //TODO : remove self from messaging list
+        return new NotificationMessage(ServerMessage.ServerMessageType.NOTIFICATION, username + "has left the game. Adios!");
     }
 
     private ServerMessage makeMove(UserGameCommand command){
@@ -45,5 +63,6 @@ public class WebsocketServices extends BaseService{
     private ServerMessage resign(UserGameCommand command){
 
     }
+
 
 }
