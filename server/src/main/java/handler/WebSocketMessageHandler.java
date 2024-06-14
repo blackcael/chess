e880d53479dd245516606b217978f4a28223d869
blackcael <blackcael@gmail.com>
2024-06-13 18:14:55 -0600

    update

--- server/src/main/java/handler/WebSocketMessageHandler.java
+++ server/src/main/java/handler/WebSocketMessageHandler.java
@@ -8,11 +8,15 @@ import websocket.messages.ServerMessage;
 public class WebSocketMessageHandler {
     public static String handleMessage(Database database, String message){
         //1. parse down the message, figure out which type of command
-        UserGameCommand userGameCommand; //parseThatHomie
+        UserGameCommand userGameCommand = messageToCommand(message);
         WebsocketServices websocketService = new WebsocketServices(database);
         ServerMessage serverMessage = websocketService.service(userGameCommand);
         //turn servermessage back into a string and send it back
 
         return "peepee";
+    }
+
+    private static UserGameCommand messageToCommand(String message){
+        return new UserGameCommand("peepee");
     }
 }
