6d3f1583cde17ad59eab8fd1752354741e02761c
blackcael <blackcael@gmail.com>
2024-06-13 18:03:44 -0600

    createdService.makeMove(unfinished)

--- server/src/main/java/service/WebsocketServices.java
+++ server/src/main/java/service/WebsocketServices.java
@@ -1,46 +1,51 @@
 package service;
 
 import chess.ChessGame;
+import chess.InvalidMoveException;
 import dataaccess.DataAccessException;
 import dataaccess.Database;
 import intermediary.InvalidAuthException;
 import model.GameData;
 import server.Server;
-import websocket.commands.LeaveCommand;
-import websocket.commands.UserGameCommand;
+import websocket.commands.*;
 import websocket.messages.ErrorMessage;
+import websocket.messages.LoadGameMessage;
 import websocket.messages.NotificationMessage;
 import websocket.messages.ServerMessage;
 
 public class WebsocketServices extends BaseService{
-
+    private String username;
+    private ChessGame.TeamColor playerColor;
     public WebsocketServices(Database database) {
         super(database);
     }
 
     public ServerMessage service(UserGameCommand command){
+
         try {
             validateAuthToken(command.getAuthString());
+            username = authDataBase.getAuth(command.getAuthString()).username();
             return switch (command.getCommandType()) {
-                case CONNECT -> connect(command);
-                case LEAVE -> leave(command);
-                case MAKE_MOVE -> makeMove(command);
-                case RESIGN -> resign(command);
+                case CONNECT -> connect((ConnectCommand) command);
+                case LEAVE -> leave((LeaveCommand) command);
+                case MAKE_MOVE -> makeMove((MakeMoveCommand) command);
+                case RESIGN -> resign((ResignCommand) command);
             };
         }catch (InvalidAuthException e) {
             return new ErrorMessage(ServerMessage.ServerMessageType.ERROR, "Error: Invalid AuthToken");
         } catch (DataAccessException e) {
             return new ErrorMessage(ServerMessage.ServerMessageType.ERROR, "Error: Data Access Exception");
-        }
+        } catch (InvalidMoveException e) {
+            return new ErrorMessage(ServerMessage.ServerMessageType.ERROR, "Error: Invalid Move!");
+    }
     }
 
-    private ServerMessage connect(UserGameCommand command){
+    private ServerMessage connect(ConnectCommand command){
         //honestly not super sure what to do here...
-
+        //mostly just map work?
     }
 
     private ServerMessage leave(LeaveCommand leaveCommand) throws DataAccessException {
-        String username = authDataBase.getAuth(leaveCommand.getAuthString()).username();
         GameData gameData = gameDataBase.getGame(leaveCommand.getGameID());
         String updatedWhiteUsername = gameData.whiteUsername();
         String updatedBlackUsername = gameData.blackUsername();
@@ -56,11 +61,17 @@ public class WebsocketServices extends BaseService{
         return new NotificationMessage(ServerMessage.ServerMessageType.NOTIFICATION, username + "has left the game. Adios!");
     }
 
-    private ServerMessage makeMove(UserGameCommand command){
-
+    private ServerMessage makeMove(MakeMoveCommand makeMoveCommand) throws DataAccessException, InvalidMoveException {
+        //color issues?
+        GameData gameData = gameDataBase.getGame(makeMoveCommand.getGameID());
+        ChessGame updatedGame = gameData.game();
+        updatedGame.makeMove(makeMoveCommand.getMove());
+        GameData updatedGameData =new GameData(gameData.gameID(), gameData.whiteUsername(), gameData.blackUsername(), gameData.gameName(), updatedGame);
+        gameDataBase.updateGame(updatedGameData);
+        return new LoadGameMessage(ServerMessage.ServerMessageType.LOAD_GAME, updatedGame);
     }
 
-    private ServerMessage resign(UserGameCommand command){
+    private ServerMessage resign(ResignCommand command){
 
     }
 
